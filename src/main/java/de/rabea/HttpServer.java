094fc54febe1e240fe1df4968075946b6237eeda
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-26 16:22:47 +0100

    removes superflous classes for threading setup

--- src/main/java/de/rabea/HttpServer.java
+++ src/main/java/de/rabea/HttpServer.java
@@ -3,6 +3,7 @@ package de.rabea;
 import de.rabea.communication.SocketReader;
 import de.rabea.communication.SocketWriter;
 import de.rabea.exceptions.ServerSocketException;
+import de.rabea.exceptions.SocketException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,29 +11,22 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class HttpServer {
+public class HttpServer  implements Runnable {
     private final ServerSocket serverSocket;
+    private Router router;
     private final ExecutorService executorService;
-    private final int POOL_SIZE = 20;
 
-    public HttpServer(ExecutorServiceFactory executorServiceFactory, ServerSocket serverSocket) {
-        this.executorService = executorServiceFactory.create(POOL_SIZE);
+    public HttpServer(ExecutorService executorService, ServerSocket serverSocket, Router router) {
         this.serverSocket = serverSocket;
+        this.router = router;
+        this.executorService = executorService;
     }
 
-    public void start(Router router) {
+    public void start() {
         while (true) {
-            execute(router);
-        }
-    }
-
-    public void execute(Router router) {
-        try {
-            executeServerRunnerInThread(router);
-        } catch (IOException e) {
-            shutdown();
-            throw new ServerSocketException("Cannot create Socket" + e.getMessage());
+            run();
         }
     }
 
@@ -40,13 +34,29 @@ public class HttpServer {
         executorService.shutdown();
     }
 
-    private void executeServerRunnerInThread(Router router) throws IOException {
-        Socket socket = serverSocket.accept();
-        executorService.execute(new HttpServerRunner(
-                new ServerWorkerFactory(
-                        new SocketReader(new BufferedReader(new InputStreamReader(socket.getInputStream()))),
-                        new SocketWriter(socket),
-                        router
-                )));
+    @Override
+    public void run() {
+        Socket socket;
+        try {
+            socket = createSocket();
+        } catch (IOException e) {
+            throw new ServerSocketException("cannot create socket " + e.getMessage());
+        }
+        try {
+            startServerWorker(socket);
+        } catch (IOException e) {
+           throw new SocketException("cannot get input stream " + e.getMessage());
+        }
+    }
+
+    private Socket createSocket() throws IOException {
+        return serverSocket.accept();
+    }
+
+    private void startServerWorker(Socket socket) throws IOException {
+        new ServerWorker(
+                new SocketReader(new BufferedReader(new InputStreamReader(socket.getInputStream()))),
+                new SocketWriter(socket),
+                router).start();
     }
 }
--- src/main/java/de/rabea/Main.java
+++ src/main/java/de/rabea/Main.java
@@ -4,6 +4,7 @@ import de.rabea.controller.RootController;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,10 +14,10 @@ public class Main {
 
         System.out.println("Server started at port " + port + " and directory " + directory);
         ServerSocket serverSocket = new ServerSocket(port);
-        HttpServer httpServer = new HttpServer(new ThreadPoolExecutorServiceFactory(), serverSocket);
         Router router = new Router();
         router.configure("/", new RootController());
         router.configure("/form", new FormController());
-        httpServer.start(router);
+        HttpServer httpServer = new HttpServer(Executors.newFixedThreadPool(20), serverSocket, router);
+        httpServer.start();
     }
 }
