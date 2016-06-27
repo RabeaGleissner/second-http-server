07c1a420108ff63b651d58c54c3436f0c23b4bbb
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-27 16:10:32 +0100

    adds response for post request to FormController

--- src/main/java/de/rabea/FormController.java
+++ src/main/java/de/rabea/FormController.java
@@ -1,17 +1,33 @@
 package de.rabea;
 
 import de.rabea.request.HttpRequest;
+import de.rabea.request.HttpVerb;
 import de.rabea.response.HttpResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static de.rabea.request.HttpVerb.*;
 import static de.rabea.request.HttpVerb.PUT;
 
 public class FormController extends Controller {
 
+    Map<HttpVerb, HttpResponse> responsesForMethods = new HashMap<>();
+
+    public FormController() {
+        this.responsesForMethods = registerResponses();
+    }
+
     @Override
     public HttpResponse getResponse(HttpRequest request) {
-        if (request.requestLine().method() == PUT) {
-            return ok200();
-        }
-        return methodNotAllowed();
+        return responsesForMethods.getOrDefault(request.requestLine().method(),
+                methodNotAllowed());
+    }
+
+    private Map<HttpVerb, HttpResponse> registerResponses() {
+        HttpResponse okResponse = ok200();
+        responsesForMethods.put(PUT, okResponse);
+        responsesForMethods.put(POST, okResponse);
+        return responsesForMethods;
     }
 }
