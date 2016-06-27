2f2cb5d247787bf9e67e40774133e8176fc93ec8
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-27 20:41:26 +0100

    removes methods from Controller abstract class

--- src/main/java/de/rabea/controller/RootController.java
+++ src/main/java/de/rabea/controller/RootController.java
@@ -4,16 +4,21 @@ import de.rabea.Controller;
 import de.rabea.request.HttpRequest;
 import de.rabea.request.HttpVerb;
 import de.rabea.response.HttpResponse;
+import de.rabea.response.ResponseCreator;
+import de.rabea.response.creator.GetResponseCreator;
+import de.rabea.response.creator.HeadResponseCreator;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static de.rabea.request.HttpVerb.GET;
 import static de.rabea.request.HttpVerb.HEAD;
+import static de.rabea.response.head.StatusLine.NOT_ALLOWED;
+import static de.rabea.response.head.StatusLine.OK;
 
 public class RootController extends Controller {
 
-    private Map<HttpVerb, HttpResponse> responsesForMethods = new HashMap<>();
+    private Map<HttpVerb, ResponseCreator> responsesForMethods = new HashMap<>();
 
     public RootController() {
         this.responsesForMethods = registerResponses();
@@ -22,13 +27,12 @@ public class RootController extends Controller {
     @Override
     public HttpResponse getResponse(HttpRequest httpRequest) {
         return responsesForMethods.getOrDefault(httpRequest.requestLine().method(),
-                methodNotAllowed());
+                new GetResponseCreator(NOT_ALLOWED)).create(httpRequest.body());
     }
 
-    private Map<HttpVerb, HttpResponse> registerResponses() {
-        HttpResponse okResponse = ok200();
-        responsesForMethods.put(GET, okResponse);
-        responsesForMethods.put(HEAD, okResponse);
+    private Map<HttpVerb, ResponseCreator> registerResponses() {
+        responsesForMethods.put(GET, new GetResponseCreator(OK));
+        responsesForMethods.put(HEAD, new HeadResponseCreator(OK));
         return responsesForMethods;
     }
 }
