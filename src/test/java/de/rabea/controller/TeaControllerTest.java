2a377247152b7a2166fbed330a19cf67f4cd325b
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-28 18:18:48 +0100

    adds test for TeaController

---
+++ src/test/java/de/rabea/controller/TeaControllerTest.java
@@ -0,0 +1,18 @@
+package de.rabea.controller;
+
+import de.rabea.request.HttpRequest;
+import de.rabea.response.HttpResponse;
+import org.junit.Test;
+
+import static de.rabea.request.HttpVerb.GET;
+import static org.junit.Assert.assertEquals;
+
+public class TeaControllerTest {
+
+    @Test
+    public void returnResponse() {
+        TeaController controller = new TeaController();
+        HttpResponse response = controller.getResponse(new HttpRequest(GET, "/"));
+        assertEquals("HTTP/1.1 200 OK\n\n", response.asString());
+    }
+}
