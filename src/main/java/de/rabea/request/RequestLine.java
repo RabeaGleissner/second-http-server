7d1328a9afbdd362aab4024c430fdae7b713ee26
Rabea Gleissner <rabeagleissner@gmail.com>
2016-06-30 18:06:19 +0100

    returns url without params

--- src/main/java/de/rabea/request/RequestLine.java
+++ src/main/java/de/rabea/request/RequestLine.java
@@ -16,6 +16,22 @@ public class RequestLine {
         return firstLine.split(" ")[1];
     }
 
+    public String route() {
+        String uri = uri();
+        if (hasParams(uri)) {
+            return uriWithoutParams(uri);
+        }
+        return uri;
+    }
+
+    private String uriWithoutParams(String uri) {
+        return uri.substring(0, uri.indexOf("?"));
+    }
+
+    private boolean hasParams(String uri) {
+        return uri.contains("?");
+    }
+
     public String protocol() {
         return firstLine.split(" ")[2];
     }
@@ -27,4 +43,5 @@ public class RequestLine {
     private String getFirstLine(String rawRequest) {
         return rawRequest.split("\n")[0];
     }
+
 }
