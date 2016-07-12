66c5e785a5a2a797faf60ef799c25d91ce1163a3
Rabea Gleissner <rabeagleissner@gmail.com>
2016-07-12 18:01:17 +0100

    creates HTML for menu page

--- src/main/java/de/rabea/controller/TicTacToeMenuController.java
+++ src/main/java/de/rabea/controller/TicTacToeMenuController.java
@@ -5,8 +5,10 @@ import de.rabea.controller.html.MenuHtml;
 import de.rabea.controller.html.TicTacToeHtmlGenerator;
 import de.rabea.request.HttpRequest;
 import de.rabea.response.HttpResponse;
+import de.rabea.response.head.RedirectResponseHeader;
 
 import static de.rabea.response.head.StatusLine.OK;
+import static de.rabea.response.head.StatusLine.REDIRECT;
 
 public class TicTacToeMenuController extends Controller {
 
@@ -14,5 +16,10 @@ public class TicTacToeMenuController extends Controller {
     public HttpResponse doGet(HttpRequest request) {
         String html = new TicTacToeHtmlGenerator(new MenuHtml()).generate();
         return new HttpResponse(OK, html.getBytes());
+    }
+
+    @Override
+    public HttpResponse doPost(HttpRequest request) {
+        return new HttpResponse(REDIRECT, new RedirectResponseHeader("http://localhost:5000/ttt-game"));
     }
 }
